9579e41cdbc3b57df614f75b70d4281f620fb2bb
Antohan <justanton19@gmail.com>
2018-10-22 19:09:26 +0700

    Improved validation for register an user. Added validation for login.

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load User model
 const User = require('../../models/User');
@@ -65,8 +66,14 @@ router.post('/register', (req, res) => {
 // @desc    Login User / Returning JWT Token
 // @access  Public
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
   const email = req.body.email;
   const password = req.body.password;
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
   // Find user by email
   User
@@ -74,7 +81,8 @@ router.post('/login', (req, res) => {
     .then(user => {
       // Check for user
       if (!user) {
-        return res.status(404).json({ email: 'User not found' });
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
       }
 
       // Check Password
@@ -98,7 +106,8 @@ router.post('/login', (req, res) => {
                 });
               });
           } else {
-            return res.status(400).json({ password: 'Password incorrect' });
+            errors.password = 'Password incorrect';
+            return res.status(400).json(errors);
           }
         });
     })
