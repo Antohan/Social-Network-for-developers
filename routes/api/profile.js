f30f874bc89391479195dc83e9b4772a6084eab8
Antohan <justanton19@gmail.com>
2018-10-24 12:02:12 +0700

    Created API for adding an education and an experience to profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
-const validateProfileInout = require('../../validation/profile');
+const validateProfileInput = require('../../validation/profile');
+const validateExperienceInput = require('../../validation/experience');
+const validateEducationInput = require('../../validation/education');
 
 // Load Models
 const Profile = require('../../models/Profile');
@@ -104,7 +106,7 @@ router.post(
   '/',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
-    const { errors, isValid } = validateProfileInout(req.body);
+    const { errors, isValid } = validateProfileInput(req.body);
 
     if (!isValid) {
       return res.status(400).json(errors);
@@ -181,6 +183,74 @@ router.post(
             });
         }
       });
+  }
+);
+
+// @route   POST api/profile/experience
+// @desc    Add experience to profile
+// @access  Privet
+router.post(
+  '/experience',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateExperienceInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    Profile
+      .findOne({ user: req.user.id })
+      .then(profile => {
+        const newExp = {
+          title: req.body.title,
+          company: req.body.company,
+          location: req.body.location,
+          from: req.body.from,
+          to: req.body.to,
+          current: req.body.current,
+          description: req.body.description
+        };
+
+        // Add to exp array
+        profile.experience.unshift(newExp);
+
+        profile.save().then(profile => res.json(profile));
+      })
+  }
+);
+
+// @route   POST api/profile/education
+// @desc    Add education to profile
+// @access  Privet
+router.post(
+  '/education',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateEducationInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    Profile
+      .findOne({ user: req.user.id })
+      .then(profile => {
+        const newEdu = {
+          school: req.body.school,
+          degree: req.body.degree,
+          fieldofstudy: req.body.fieldofstudy,
+          from: req.body.from,
+          to: req.body.to,
+          current: req.body.current,
+          description: req.body.description
+        };
+
+        // Add to exp array
+        profile.education.unshift(newEdu);
+
+        profile.save().then(profile => res.json(profile));
+      })
   }
 );
 
