456d80777a8146fd809f1343faabb84373a15dcf
Antohan <justanton19@gmail.com>
2018-10-24 12:51:16 +0700

    Created API for delete an experience, an aducation, user and his profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -254,4 +254,73 @@ router.post(
   }
 );
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Detele experience from profile by ID
+// @access  Privet
+router.delete(
+  '/experience/:exp_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+
+    Profile
+      .findOne({ user: req.user.id })
+      .then(profile => {
+        // Ger remove index
+        const removeIndex = profile.experience
+          .map(item => item.id)
+          .indexOf(req.params.exp_id);
+
+        // Splice out of array
+        profile.experience.splice(removeIndex, 1);
+
+        // Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    Detele education from profile by ID
+// @access  Privet
+router.delete(
+  '/education/:edu_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+
+    Profile
+      .findOne({ user: req.user.id })
+      .then(profile => {
+        // Ger remove index
+        const removeIndex = profile.education
+          .map(item => item.id)
+          .indexOf(req.params.edu_id);
+
+        // Splice out of array
+        profile.education.splice(removeIndex, 1);
+
+        // Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   DELETE api/profile/profile
+// @desc    Detele user and profile
+// @access  Privet
+router.delete(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile
+      .findOneAndRemove({ user: req.user.id })
+      .then(() => {
+        User
+          .findOneAndRemove({ _id: req.user.id })
+          .then(() => res.json({ success: truw }));
+      });
+  }
+);
+
 module.exports = router;
