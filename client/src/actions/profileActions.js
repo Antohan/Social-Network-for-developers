95ee5d7d52d85ce7db7f5392c8d2bf61c94cf8c5
Antohan <justanton19@gmail.com>
2018-11-01 13:30:55 +0700

    Added Profile components.

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -18,6 +18,21 @@ export const getCurrentProfile = () => dispatch => {
     }));
 };
 
+export const getProfileByHandle = (handle) => dispatch => {
+  dispatch(setProfileLoading());
+
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res => dispatch({
+      type: actionTypes.GET_PROFILE,
+      payload: res.data
+    }))
+    .catch(err => dispatch({
+      type: actionTypes.GET_PROFILE,
+      payload: null
+    }));
+};
+
 export const createProfile = (profileData, history) => dispatch => {
   axios
     .post('/api/profile', profileData)
