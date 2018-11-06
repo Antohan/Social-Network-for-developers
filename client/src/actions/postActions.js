e20340ba372b054713ac348f653908d0b3183f83
Antohan <justanton19@gmail.com>
2018-11-06 22:30:17 +0700

    Add Comment Form Component for a post page.

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import * as actionTypes from './types';
 
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors());
   axios
     .post('/api/posts', postData)
     .then(res => dispatch({
@@ -76,8 +77,41 @@ export const removeLike = id => dispatch => {
     }));
 };
 
+export const addComment = (postId, commentData) => dispatch => {
+  dispatch(clearErrors());
+  axios
+    .post(`/api/posts/comment/${postId}`, commentData)
+    .then(res => dispatch({
+      type: actionTypes.GET_POST,
+      payload: res.data
+    }))
+    .catch(err => dispatch({
+      type: actionTypes.GET_ERRORS,
+      payload: err.response.data
+    }));
+};
+
+export const deleteComment = (postId, commentId) => dispatch => {
+  axios
+    .delete(`/api/posts/comment/${postId}/${commentId}`)
+    .then(res => dispatch({
+      type: actionTypes.GET_POST,
+      payload: res.data
+    }))
+    .catch(err => dispatch({
+      type: actionTypes.GET_ERRORS,
+      payload: err.response.data
+    }));
+};
+
 export const setPostLoading = () => {
   return {
     type: actionTypes.POST_LOADING
+  };
+};
+
+export const clearErrors = () => {
+  return {
+    type: actionTypes.CLEAR_ERRORS
   };
 };
--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -1,11 +1,77 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+
+import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
+import { addComment } from '../../actions/postActions';
 
 class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      errors: {}
+    };
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.errors) {
+      this.setState({ errors: newProps.errors });
+    }
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+
+    const { user } = this.props.auth;
+    const { postId } = this.props;
+
+    const newComment = {
+      text: this.state.text,
+      name: user.name,
+      avatar: user.avatar
+    };
+
+    this.props.addComment(postId, newComment);
+    this.setState({ text: '' });
+  }
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
   render() {
+    const { text, errors } = this.state;
+
     return (
-      <div>CommentForm</div>
+      <div className="post-form mb-3">
+        <div className="card card-info">
+          <div className="card-header bg-info text-white">
+            Make a comment...
+          </div>
+          <div className="card-body">
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <TextAreaFieldGroup
+                  placeholder="Reply to post"
+                  name="text"
+                  value={text}
+                  onChange={this.onChange}
+                  error={errors.text} />
+              </div>
+              <button type="submit" className="btn btn-dark">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     );
   }
 }
 
-export default CommentForm;
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, { addComment })(CommentForm);
